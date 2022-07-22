e12dd2c0c89af6a4aabe49e05ef2da19cdac152d
Thiện <phamthien3001@gmail.com>
2022-07-22 20:09:42 +0700

    no message

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('shop/home');
 });
 Route::get('/admin', function () {
     return view('admin/home');
